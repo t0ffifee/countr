e344932550f95009210f1c1d5f1e834dba6ca325
ToufikAmmoura <toufikammoura@gmail.com>
2021-01-19 16:25:40 +0100

    eventcards worden nu vanuit EventCard gemaakt

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'screens/countdown_page.dart';
 import 'constants/constants.dart';
 import 'widgets/FancyFab.dart';
+import 'widgets/EventCard.dart';
 
 // TODO maak aparte widget files voor belangrijke widgets die je vaak gebruikt of groot zijn
 
@@ -36,7 +37,9 @@ class MyAppState extends State<MyApp> {
             padding: const EdgeInsets.all(8),
             children: <Widget>[
               topContainer,
-              cardInContainer(context, Colors.blue),
+              cardMaker(context, "Verjaardag Henk", "Mijn beste vriend Henk is jarig", Icons.cake),
+              cardMaker(context, "Kinderboerderij", "Mijn kinderboerderij openen", Icons.agriculture),
+              // cardInContainer(context, Colors.blue),
               // cardInContainer(context, Colors.purple),
               // cardInContainer(context, Colors.red),
               // cardInContainer(context, Colors.yellow),
@@ -78,173 +81,13 @@ Container cardInContainer(BuildContext context, Color color) {
         ),
       ),
     ]),
-    child: getCard(context),
+    child: cardMaker(context, "Titel hai", "Beschrijving", Icons.accessibility),
   );
 }
 
-// TODO dit moet naar de EventCard.dart file gaan
-Card getCard(BuildContext context) {
-  return Card(
-    // elevation: 3.0,
-    color: Color.fromRGBO(40, 40, 40, 1),
-    child: InkWell(
-      splashColor: Color.fromRGBO(190, 129, 248, 1),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => SecondRoute()),
-        );
-      },
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: Icon(
-              Icons.directions_car,
-              color: Color.fromRGBO(143, 143, 143, 1),
-            ),
-            title: Text(
-              'Perlarmo memoro',
-              style: TextStyle(
-                color: Color.fromRGBO(143, 143, 143, 1),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: Text(
-              'Lorem ipsum dolor sit amet',
-              style: TextStyle(color: Color.fromRGBO(143, 143, 143, 1)),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 40, right: 40),
-            child: Row(
-              children: [
-                RichText(
-                  text: TextSpan(
-                    text: '3',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: whiteTextColor,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: '\nDAYS',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 13,
-                          )),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: '4',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: whiteTextColor,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: '\nHOURS',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 13,
-                          )),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: '20',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: whiteTextColor,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: '\nMINUTES',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 13,
-                          )),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: '27',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: whiteTextColor,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: '\nSECONDS',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 13,
-                          )),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-              // mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            ),
-          ),
-          ButtonBar(
-            children: <Widget>[
-              FlatButton(
-                splashColor: ligthPurple.withAlpha(150),
-                onPressed: () {
-                  print("Edit button");
-                },
-                child: Icon(
-                  Icons.edit,
-                  color: whiteTextColor,
-                ),
-              ),
-              FlatButton(
-                splashColor: ligthPurple.withAlpha(150),
-                child: Icon(
-                  Icons.drag_handle,
-                  color: whiteTextColor,
-                ),
-                onPressed: () {
-                  print("Move button");
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
-    ),
-  );
+Card cardMaker(BuildContext context, String title, String description, IconData icon) {
+  EventCard eCard = new EventCard();
+  DateTime now = DateTime.now();
+  Card card = eCard.makeEventCard(context, title, description, icon, now);
+  return card;
 }
-
-// Route _createRoute() {
-//   return PageRouteBuilder(
-//     pageBuilder: (context, animation, secondaryAnimation) => CreatePage(),
-//     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-//       var begin = Offset(0.0, 1.0);
-//       var end = Offset.zero;
-//       var curve = Curves.easeOut;
-
-//       var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-//       return SlideTransition(
-//         position: animation.drive(tween),
-//         child: child,
-//       );
-//     },
-//   );
-// }
