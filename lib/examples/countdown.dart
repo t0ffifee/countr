c3f5e230a4ca0414c19ce1327bd046aa193c28de
ToufikAmmoura <toufikammoura@gmail.com>
2021-01-22 01:45:04 +0100

    countdown library toegevoegd

---
+++ lib/examples/countdown.dart
@@ -0,0 +1,227 @@
+// import 'package:example/countdown_page.dart';
+// import 'package:example/countdown_timer_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
+import 'package:flutter_countdown_timer/index.dart';
+
+// ============== MAIN ==============
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildItem("countdown", CountdownPage()),
+          buildItem("countdown page", CountdownTimerPage()),
+        ],
+      ),
+    );
+  }
+
+  Widget buildItem(String title, Widget page) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+          return page;
+        }));
+      },
+      child: Container(
+        margin: const EdgeInsets.all(5),
+        color: Colors.blue,
+        width: double.infinity,
+        alignment: Alignment.center,
+        height: 100,
+        child: Text(
+          title,
+          style: TextStyle(fontSize: 36),
+        ),
+      ),
+    );
+  }
+}
+
+// ============== COUNTDOWN TIMER PAGE ==============
+
+class CountdownTimerPage extends StatefulWidget {
+  @override
+  _CountdownTimerPageState createState() => _CountdownTimerPageState();
+}
+
+class _CountdownTimerPageState extends State<CountdownTimerPage> {
+  CountdownTimerController controller;
+  int endTime = DateTime.now().millisecondsSinceEpoch + 1000 * 259200;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CountdownTimerController(endTime: endTime, onEnd: onEnd);
+    Future.delayed(Duration(seconds: 5), () {
+      setState(() {
+        endTime = DateTime.now().millisecondsSinceEpoch + 1000 * 259200;
+        controller.endTime = endTime;
+      });
+    });
+  }
+
+  void onEnd() {
+    print('onEnd');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          CountdownTimer(
+            textStyle: TextStyle(
+              fontSize: 30,
+              color: Colors.red,
+            ),
+            onEnd: onEnd,
+            endTime: endTime,
+          ),
+          CountdownTimer(
+            controller: controller,
+            widgetBuilder: (_, CurrentRemainingTime time) {
+              if (time == null) {
+                return Text('Game over');
+              }
+              return Column(
+                children: [Text("${time.days}"), Text("${time.hours}"), Text("${time.min}"), Text("${time.sec}")],
+              );
+
+              // return Text('days: [ ${time.days} ], hours: [ ${time.hours} ], min: [ ${time.min} ], sec: [ ${time.sec} ]');
+            },
+          ),
+          CountdownTimer(
+            controller: controller,
+            widgetBuilder: (BuildContext context, CurrentRemainingTime time) {
+              if (time == null) {
+                return Text('Game over');
+              }
+              List<Widget> list = [];
+              if (time.days != null) {
+                list.add(Row(
+                  children: <Widget>[
+                    Icon(Icons.sentiment_dissatisfied),
+                    Text(time.days.toString()),
+                  ],
+                ));
+              }
+              if (time.hours != null) {
+                list.add(Row(
+                  children: <Widget>[
+                    Icon(Icons.sentiment_satisfied),
+                    Text(time.hours.toString()),
+                  ],
+                ));
+              }
+              if (time.min != null) {
+                list.add(Row(
+                  children: <Widget>[
+                    Icon(Icons.sentiment_very_dissatisfied),
+                    Text(time.min.toString()),
+                  ],
+                ));
+              }
+              if (time.sec != null) {
+                list.add(Row(
+                  children: <Widget>[
+                    Icon(Icons.sentiment_very_satisfied),
+                    Text(time.sec.toString()),
+                  ],
+                ));
+              }
+
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: list,
+              );
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.stop),
+        onPressed: () {
+          onEnd();
+          controller.disposeTimer();
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+// ============== COUNTDOWN PAGE ==============
+
+class CountdownPage extends StatefulWidget {
+  @override
+  _CountdownPageState createState() => _CountdownPageState();
+}
+
+class _CountdownPageState extends State<CountdownPage> {
+  CountdownController countdownController = CountdownController(duration: Duration(minutes: 1));
+  bool isRunning = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Countdown(
+            countdownController: countdownController,
+            builder: (_, Duration time) {
+              return Text(
+                '${time?.inSeconds ?? 0}',
+                style: TextStyle(fontSize: 80),
+              );
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(isRunning ? Icons.stop : Icons.play_arrow),
+        onPressed: () {
+          if (!countdownController.isRunning) {
+            countdownController.start();
+            setState(() {
+              isRunning = true;
+            });
+          } else {
+            countdownController.stop();
+            setState(() {
+              isRunning = false;
+            });
+          }
+        },
+      ),
+    );
+  }
+}
