f634086d5ee376e63b7098991d4a48c211a3777c
Toufik Ammoura <toufikammoura@gmail.com>
2021-01-24 10:19:47 +0100

    change

--- lib/widgets/EventCard.dart
+++ lib/widgets/EventCard.dart
@@ -9,33 +9,9 @@ class EventCard {
   final Event event;
 
   EventCard(this.onTap, this.event);
-  // Getters en Setters nodig denk ik
-
-  // Een functie maken die een Card kan uitspugen die we kunnen opslaan en daarna kunnen laten zien op het hoofdscherm
-
-  // TODO dit is volgens mij niet meer nodig sinds we een countdown library gebruiken
-  List<String> restingTime(DateTime eventDate) {
-    final now = DateTime.now();
-    var total = eventDate.difference(now).inSeconds;
-
-    final days = (total / 86400).floor();
-    total -= days * 86400;
-
-    final hours = (total / 3600).floor();
-    total -= hours * 3600;
-
-    final minutes = (total / 60).floor();
-    total -= minutes * 60;
-
-    final seconds = total % 60;
-
-    List<int> list = [days, hours, minutes, seconds];
-    return list.map((e) => e.toString()).toList();
-  }
 
   Widget getCard(BuildContext context) {
     // TODO dit moet er natuurlijk anders uit gaan zien omdat je een CardCountDown gebruikt
-    List<String> times = restingTime(event.eDate);
     return Card(
       // elevation: 3.0,
       color: Color.fromRGBO(40, 40, 40, 1),
@@ -50,7 +26,8 @@ class EventCard {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            topBarCard(event.eTitle, event.eDescription, IconData(event.eIconCodePoint, fontFamily: 'MaterialIcons')),
+            topBarCard(event.eTitle, event.eDescription,
+                IconData(event.eIconCodePoint, fontFamily: 'MaterialIcons')),
             CardCountDown(event),
             cardButtonBar(),
           ],
@@ -59,53 +36,26 @@ class EventCard {
     );
   }
 
-  // TODO deze functie is uiteindelijk niet meer nodig
-  Widget makeEventCardFromEvent(BuildContext context, Event event) {
-    List<String> times = restingTime(event.eDate);
-    return Card(
-      // elevation: 3.0,
-      color: Color.fromRGBO(40, 40, 40, 1),
-      child: InkWell(
-        splashColor: lightPurple.withOpacity(0.4),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CountDownPage(event)),
-          );
-        },
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            topBarCard(event.eTitle, event.eDescription, IconData(event.eIconCodePoint, fontFamily: 'MaterialIcons')),
-            dateShower(times), // hier geef je natuurlijk de resterende dagen, uren, minuten en seconden
-            cardButtonBar(),
-          ],
-        ),
-      ),
-    );
-  }
-
   // TODO deze dingen hieronder horen duidelijk een eigen class te zijn
-  Widget topBarCard(String title, String description, IconData icon) {
-    return ListTile(
-      leading: Icon(
-        icon,
-        color: Color.fromRGBO(143, 143, 143, 1),
-      ),
-      title: Text(
-        title,
-        style: TextStyle(
+  Widget topBarCard(String title, String description, IconData icon) =>
+      ListTile(
+        leading: Icon(
+          icon,
           color: Color.fromRGBO(143, 143, 143, 1),
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
         ),
-      ),
-      subtitle: Text(
-        description,
-        style: TextStyle(color: Color.fromRGBO(143, 143, 143, 1)),
-      ),
-    );
-  }
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Color.fromRGBO(143, 143, 143, 1),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          description,
+          style: TextStyle(color: Color.fromRGBO(143, 143, 143, 1)),
+        ),
+      );
 
   // TODO dit wordt uiteindelijk de CardCountDown widget
   Widget dateShower(List<String> times) {
