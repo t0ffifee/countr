6864e0c7b36b2e6198a68c16aa2e164a33c72404
ToufikAmmoura <toufikammoura@gmail.com>
2021-07-19 15:56:57 +0200

    beter paars

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // COLORS
-const Color lightPurple = Color.fromRGBO(190, 129, 248, 1);
+const Color lightPurple = Color.fromRGBO(187, 134, 252, 1);
 const Color backgroundBlack = Color.fromRGBO(18, 18, 18, 1);
 const Color lighterBlackOne = Color.fromRGBO(41, 41, 41, 1);
 const Color whiteTextColor = Color.fromRGBO(180, 180, 180, 1);
