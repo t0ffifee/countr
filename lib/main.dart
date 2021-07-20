055ceecbba4a734e11ea768d9720d72f6083b833
ToufikAmmoura <toufikammoura@gmail.com>
2021-07-20 13:57:54 +0200

    nu echt reorderable list verwijderd

--- lib/main.dart
+++ lib/main.dart
@@ -35,68 +35,37 @@ class MyApp extends StatefulWidget {
 // EventCard(event: Event(DateTime(2021, 10, 5), 'Tandarts', 'Gebits controle', Icons.alarm.codePoint, Colors.purple.shade300.value, false))
 
 class MyAppState extends State<MyApp> {
-  final List<Widget> myItems = [
-    Container(color: Colors.blue, height: 100, width: 50, key: UniqueKey()),
-    Container(color: Colors.red, height: 100, width: 50, key: UniqueKey()),
-    Container(color: Colors.orange, height: 100, width: 50, key: UniqueKey()),
-    Container(color: Colors.purple, height: 100, width: 50, key: UniqueKey())
-  ];
-
-  var _controller = ScrollController();
-  var _visible = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(() {
-      if (_controller.position.atEdge) {
-        if (_controller.position.pixels > 0) if (_visible)
-          setState(() {
-            _visible = false;
-          });
-      } else {
-        if (!_visible)
-          setState(() {
-            _visible = true;
-          });
-      }
-    });
-  }
-
   @override
   Widget build(context) {
     print("[BUILD] MyAppState is gebouwd");
+
+    bool _visible = true;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: ReorderableListView(
-            scrollController: _controller,
-            onReorder: (oldIndex, newIndex) {
-              setState(() {
-                if (oldIndex < newIndex) {
-                  newIndex -= 1;
-                }
-                final item = myItems.removeAt(oldIndex);
-                myItems.insert(newIndex, item);
-              });
-            },
-            children: [
-              Container(
-                key: UniqueKey(),
-                height: 20,
-              ),
-              for (final item in myItems)
-                Container(
-                  key: UniqueKey(),
-                  child: item,
-                )
-            ],
-          ),
-        ),
         backgroundColor: backgroundBlack,
         floatingActionButton: Visibility(visible: _visible, child: FancyFab()),
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        body: Center(
+          child: Container(
+            color: backgroundBlack,
+            child: ListView(
+              padding: EdgeInsets.all(10),
+              children: [
+                Divider(),
+                EventCard(
+                  event: Event(DateTime(2021, 10, 5), 'Tandarts', 'Gebits controle', Icons.alarm.codePoint, Colors.purple.shade300.value, false),
+                ),
+                Divider(),
+                EventCard(
+                  event: Event(DateTime(2021, 10, 5), 'Tandarts', 'Gebits controle', Icons.alarm.codePoint, Colors.purple.shade300.value, false),
+                ),
+                Divider(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
