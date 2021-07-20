ea3ab16582205bdf8fcb493e11ca59e1c285aa80
ToufikAmmoura <toufikammoura@gmail.com>
2021-07-20 12:53:51 +0200

    refactoring

--- lib/widgets/EventCard.dart
+++ lib/widgets/EventCard.dart
@@ -5,173 +5,134 @@ import 'CardCountDown.dart';
 import 'Event.dart';
 
 class EventCard extends StatelessWidget {
-  const EventCard({Key? key}) : super(key: key);
+  final Event event;
+
+  EventCard({required this.event});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Card(
+      elevation: 1,
+      color: Color.fromRGBO(29, 29, 29, 0.95),
+      clipBehavior: Clip.antiAlias,
+      child: Row(
+        children: [
+          Container(width: 10, height: 200, color: Color(event.color)),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                TopBar(title: event.eTitle, description: event.eDescription, icon: event.eIconCodePoint),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35, 0, 35, 0),
+                  child: DateShower(times: ['10', '23', '59', '59']),
+                ),
+                // cardButtonBar(context),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class EventCardd {
-  final Event event;
+class TopBar extends StatelessWidget {
+  final String title;
+  final String description;
+  final int icon;
 
-  EventCardd(this.event);
+  TopBar({required this.title, required this.description, required this.icon});
 
-  Card getCard(BuildContext context) {
-    return Card(
-      // elevation: 3.0,
-      color: Color.fromRGBO(40, 40, 40, 1),
-      child: InkWell(
-        splashColor: lightPurple.withOpacity(0.4),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CountDownPage(event)),
-          );
-        },
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            topBarCard(event.eTitle, event.eDescription, IconData(event.eIconCodePoint, fontFamily: 'MaterialIcons')),
-            CardCountDown(event),
-            cardButtonBar(),
-          ],
-        ),
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        IconData(icon, fontFamily: 'MaterialIcons'),
+        color: Color.fromRGBO(143, 143, 143, 1),
       ),
-    );
-  }
-
-  Widget topBarCard(String title, String description, IconData icon) => ListTile(
-        leading: Icon(
-          icon,
+      title: Text(
+        title,
+        style: TextStyle(
           color: Color.fromRGBO(143, 143, 143, 1),
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
         ),
-        title: Text(
-          title,
-          style: TextStyle(
-            color: Color.fromRGBO(143, 143, 143, 1),
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        subtitle: Text(
-          description,
-          style: TextStyle(color: Color.fromRGBO(143, 143, 143, 1)),
-        ),
-      );
-
-  Widget dateShower(List<String> times) {
-    return Container(
-      margin: EdgeInsets.only(left: 40, right: 40),
-      child: Row(
-        children: [
-          RichText(
-            text: TextSpan(
-              text: times[0],
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: whiteTextColor,
-              ),
-              children: <TextSpan>[
-                TextSpan(
-                    text: '\nDAYS',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 13,
-                    )),
-              ],
-            ),
-            textAlign: TextAlign.center,
-          ),
-          RichText(
-            text: TextSpan(
-              text: times[1],
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: whiteTextColor,
-              ),
-              children: <TextSpan>[
-                TextSpan(
-                    text: '\nHOURS',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 13,
-                    )),
-              ],
-            ),
-            textAlign: TextAlign.center,
-          ),
-          RichText(
-            text: TextSpan(
-              text: times[2],
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: whiteTextColor,
-              ),
-              children: <TextSpan>[
-                TextSpan(
-                    text: '\nMINUTES',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 13,
-                    )),
-              ],
-            ),
-            textAlign: TextAlign.center,
-          ),
-          RichText(
-            text: TextSpan(
-              text: times[3],
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: whiteTextColor,
-              ),
-              children: <TextSpan>[
-                TextSpan(
-                    text: '\nSECONDS',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 13,
-                    )),
-              ],
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
-        // mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      ),
+      subtitle: Text(
+        description,
+        style: TextStyle(color: Color.fromRGBO(143, 143, 143, 1)),
       ),
     );
   }
+}
 
-  Widget cardButtonBar() {
+class DateShower extends StatelessWidget {
+  final List<String> times;
+
+  DateShower({required this.times});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ElementShower(times: times, index: 0, unit: "DAYS"),
+          Container(width: 10),
+          ElementShower(times: times, index: 1, unit: "HOURS"),
+          Container(width: 10),
+          ElementShower(times: times, index: 2, unit: "MINUTES"),
+          Container(width: 10),
+          ElementShower(times: times, index: 3, unit: "SECONDS"),
+        ],
+      ),
+    );
+  }
+}
+
+class ElementShower extends StatelessWidget {
+  final List<String> times;
+  final int index;
+  final String unit;
+
+  ElementShower({required this.times, required this.index, required this.unit});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: times[index],
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: onSurfaceWhite,
+        ),
+        children: <TextSpan>[
+          TextSpan(
+              text: ('\n' + unit),
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 13,
+              )),
+        ],
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class CardButtonBar extends StatelessWidget {
+  const CardButtonBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return ButtonBar(
       children: <Widget>[
-        ElevatedButton(
-          // splashColor: lightPurple.withAlpha(150),
+        IconButton(
+          splashRadius: splashRadiusIconButtonCard,
+          icon: Icon(Icons.edit, color: onSurfaceWhite),
           onPressed: () {
-            print("Edit button");
-          },
-          child: Icon(
-            Icons.edit,
-            color: whiteTextColor,
-          ),
-        ),
-        ElevatedButton(
-          // splashColor: lightPurple.withAlpha(150),
-          child: Icon(
-            Icons.drag_handle,
-            color: whiteTextColor,
-          ),
-          onPressed: () {
-            print("Move button");
+            print("Edit Button");
           },
         ),
       ],
