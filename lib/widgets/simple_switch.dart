c2da95443018349dd52c005e9f4ffc29902a1661
ToufikAmmoura <toufikammoura@gmail.com>
2021-07-26 11:59:20 +0200

    checkbox ding van gemaakt

--- lib/widgets/simple_switch.dart
+++ lib/widgets/simple_switch.dart
@@ -1,28 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:countdown_app/constants/constants.dart';
 
-class SimpleSwitch extends StatefulWidget {
-  final ValueChanged<bool>? onSwitchChanged;
-  SimpleSwitch({Key? key, this.onSwitchChanged}) : super(key: key);
+class SimpleCheckBox extends StatefulWidget {
+  final ValueChanged<bool>? onCheckChanged;
+  SimpleCheckBox({Key? key, this.onCheckChanged}) : super(key: key);
 
   @override
-  SimpleSwitchState createState() => SimpleSwitchState();
+  SimpleCheckBoxState createState() => SimpleCheckBoxState();
 }
 
-class SimpleSwitchState extends State<SimpleSwitch> {
+class SimpleCheckBoxState2 extends State<SimpleCheckBox> {
+  bool v = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      title: Text('Receive notification'),
+      value: v,
+      onChanged: (value) {
+        setState(() {
+          v = value!;
+          widget.onCheckChanged!(v);
+        });
+      },
+      controlAffinity: ListTileControlAffinity.trailing,
+      secondary: Icon(Icons.notifications),
+    );
+  }
+}
+
+class SimpleCheckBoxState extends State<SimpleCheckBox> {
   bool v = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("Push Notification"),
-      leading: Switch(
-        value: v,
+      leading: Icon(Icons.notifications),
+      title: Text(
+        'Receive notification',
+        style: TextStyle(color: whiteTextColor),
+      ),
+      trailing: Checkbox(
         activeColor: lightPurple,
+        checkColor: lighterBlackOne,
+        value: v,
         onChanged: (value) {
           setState(() {
-            v = value;
-            widget.onSwitchChanged!(v);
+            v = value!;
+            widget.onCheckChanged!(v);
           });
         },
       ),
