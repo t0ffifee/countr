769b9671e250efae871be4e85b9c75dc9fdea47c
ToufikAmmoura <toufikammoura@gmail.com>
2021-07-26 11:59:29 +0200

    kleuren veranderd

--- lib/widgets/TextInput.dart
+++ lib/widgets/TextInput.dart
@@ -12,21 +12,21 @@ class TextInput extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 10),
-      child: Theme(
-        data: ThemeData(),
-        child: TextField(
-          controller: controller,
-          style: TextStyle(
+      child: TextField(
+        cursorColor: lightPurple,
+        controller: controller,
+        style: TextStyle(
+          color: whiteTextColor,
+        ),
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent)),
+          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: lightPurple)),
+          filled: true,
+          fillColor: lighterBlackOne,
+          border: OutlineInputBorder(),
+          labelText: name,
+          labelStyle: TextStyle(
             color: whiteTextColor,
-          ),
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: lighterBlackOne,
-            border: OutlineInputBorder(),
-            labelText: name,
-            labelStyle: TextStyle(
-              color: whiteTextColor,
-            ),
           ),
         ),
       ),
