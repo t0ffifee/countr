d727ef8835e084909f435f983d78fe0dc0ae5875
ToufikAmmoura <toufikammoura@gmail.com>
2021-08-25 18:41:06 +0200

    fontweights aangepast, ondertekst toegevoegd, responsive gemaakt

--- lib/screens/countdown_page.dart
+++ lib/screens/countdown_page.dart
@@ -9,45 +9,6 @@ class CountDownPage extends StatelessWidget {
 
   CountDownPage(this.event);
 
-  Widget counter(String num, String unit, BuildContext context) {
-    MediaQueryData queryData = MediaQuery.of(context);
-
-    print('MediaQuery:');
-    double height = queryData.size.height;
-    double width = queryData.size.width;
-
-    return FittedBox(
-      fit: BoxFit.fill,
-      child: RichText(
-        textAlign: TextAlign.left,
-        text: TextSpan(
-          text: '$num\t',
-          style: TextStyle(
-            // fontSize: 80,
-            color: whiteTextColor,
-          ),
-          children: <TextSpan>[
-            TextSpan(
-              text: unit,
-              style: TextStyle(
-                fontWeight: FontWeight.normal,
-                fontSize: 6,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget counterblock(String num, String unit, BuildContext context) {
-    return Expanded(
-      // width: double.infinity,
-      // margin: EdgeInsets.only(top: 40, left: 40),
-      child: counter(num, unit, context),
-    );
-  }
-
   Widget topBar(MediaQueryData mqd) {
     double height = mqd.size.height;
     double width = mqd.size.width;
@@ -76,7 +37,7 @@ class CountDownPage extends StatelessWidget {
                     child: SingleChildScrollView(
                       child: Text(
                         event.eTitle,
-                        style: TextStyle(color: whiteTextColor, fontSize: width * 0.06),
+                        style: TextStyle(color: whiteTextColor, fontSize: width * 0.06, fontWeight: FontWeight.w300),
                       ),
                     ),
                   ),
@@ -87,6 +48,81 @@ class CountDownPage extends StatelessWidget {
           Padding(
             padding: EdgeInsets.only(right: width * 0.05),
             child: Icon(Icons.fiber_manual_record_outlined, color: Color(event.eColor), size: width * 0.06),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget counter(String num, String unit, BuildContext context) {
+    MediaQueryData queryData = MediaQuery.of(context);
+
+    print('MediaQuery:');
+    double height = queryData.size.height;
+    double width = queryData.size.width;
+
+    return Padding(
+      padding: EdgeInsets.only(bottom: height * 0.02),
+      child: FittedBox(
+        fit: BoxFit.fill,
+        child: RichText(
+          textAlign: TextAlign.left,
+          text: TextSpan(
+            text: '$num\t',
+            style: TextStyle(
+              // fontSize: 80,
+              fontWeight: FontWeight.w300,
+              color: whiteTextColor,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: unit,
+                style: TextStyle(
+                  fontWeight: FontWeight.w100,
+                  fontSize: 6,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget counterblock(String num, String unit, BuildContext context) {
+    return Expanded(
+      // width: double.infinity,
+      // margin: EdgeInsets.only(top: 40, left: 40),
+      child: counter(num, unit, context),
+    );
+  }
+
+  Widget fullCounter(MediaQueryData mqd) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          // Container(
+          //   margin: EdgeInsets.only(bottom: 20),
+          //   child: EventProgress(event: event),
+          // ),
+          LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              print(constraints.maxHeight);
+              return Container(
+                height: constraints.maxHeight * 0.8,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    counterblock('20', 'D', context),
+                    counterblock('04', 'H', context),
+                    counterblock('08', 'M', context),
+                    counterblock('05', 'S', context),
+                  ],
+                ),
+              );
+            },
           ),
         ],
       ),
@@ -107,6 +143,16 @@ class CountDownPage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             topBar(queryData),
+            fullCounter(queryData),
+            Container(
+              padding: EdgeInsets.only(bottom: height * 0.02),
+              width: width * 0.9,
+              height: height * 0.11,
+              child: Text(
+                'Time is the most valuable thing a man can spend. Because it is so valuable.',
+                style: TextStyle(color: whiteTextColor, fontWeight: FontWeight.w300, fontSize: width * 0.05),
+              ),
+            )
             // Expanded(
             //   child: Row(
             //     mainAxisAlignment: MainAxisAlignment.center,
